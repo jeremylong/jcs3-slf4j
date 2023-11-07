baa2b6acc51b63ab091f755f9bf1ab2533dfed7b
Jeremy Long <jeremy.long@gmail.com>
2023-11-07 06:48:09 -0500

    fix: log messages from jcs3 have {0} (#1)

--- jcs3-slf4j/src/test/java/io/github/jeremylong/jcs3/slf4j/Slf4jAdapterTest.java
+++ jcs3-slf4j/src/test/java/io/github/jeremylong/jcs3/slf4j/Slf4jAdapterTest.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2022 Jeremy Long
+ *  Copyright 2022-2023 Jeremy Long
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -59,14 +59,14 @@ class Slf4jAdapterTest {
         Object message1 = "test-debug-1";
         String expected1 = String.format(LOG_FORMAT, "DEBUG", message);
         log.debug(message1);
-        String message2 = "test-debug-2-{}";
+        String message2 = "test-debug-2-{0}";
         String formatted2 = "test-debug-2-test";
         String expected2 = String.format(LOG_FORMAT, "DEBUG", formatted2);
         log.debug(message2, "test");
         String message3 = "test-debug-3";
         String expected3 = String.format(LOG_FORMAT, "DEBUG", message);
         log.debug(message3);
-        String message4 = "test-debug-4-{}";
+        String message4 = "test-debug-4-{0}";
         String formatted4 = "test-debug-4-test";
         String expected4 = String.format(LOG_FORMAT, "DEBUG", formatted4);
         log.debug(message4, () -> "test");
@@ -87,14 +87,14 @@ class Slf4jAdapterTest {
         Object message1 = "test-error-1";
         String expected1 = String.format(LOG_FORMAT, "ERROR", message);
         log.error(message1);
-        String message2 = "test-error-2-{}";
+        String message2 = "test-error-2-{0}";
         String formatted2 = "test-error-2-test";
         String expected2 = String.format(LOG_FORMAT, "ERROR", formatted2);
         log.error(message2, "test");
         String message3 = "test-error-3";
         String expected3 = String.format(LOG_FORMAT, "ERROR", message);
         log.error(message3);
-        String message4 = "test-error-4-{}";
+        String message4 = "test-error-4-{0}";
         String formatted4 = "test-error-4-test";
         String expected4 = String.format(LOG_FORMAT, "ERROR", formatted4);
         log.error(message4, () -> "test");
@@ -115,14 +115,14 @@ class Slf4jAdapterTest {
         Object message1 = "test-trace-1";
         String expected1 = String.format(LOG_FORMAT, "TRACE", message);
         log.trace(message1);
-        String message2 = "test-trace-2-{}";
+        String message2 = "test-trace-2-{0}";
         String formatted2 = "test-trace-2-test";
         String expected2 = String.format(LOG_FORMAT, "TRACE", formatted2);
         log.trace(message2, "test");
         String message3 = "test-trace-3";
         String expected3 = String.format(LOG_FORMAT, "TRACE", message);
         log.trace(message3);
-        String message4 = "test-trace-4-{}";
+        String message4 = "test-trace-4-{0}";
         String formatted4 = "test-trace-4-test";
         String expected4 = String.format(LOG_FORMAT, "TRACE", formatted4);
         log.trace(message4, () -> "test");
@@ -144,14 +144,14 @@ class Slf4jAdapterTest {
         Object message1 = "test-warn-1";
         String expected1 = String.format(LOG_FORMAT, "WARN ", message);
         log.warn(message1);
-        String message2 = "test-warn-2-{}";
+        String message2 = "test-warn-2-{0}";
         String formatted2 = "test-warn-2-test";
         String expected2 = String.format(LOG_FORMAT, "WARN ", formatted2);
         log.warn(message2, "test");
         String message3 = "test-warn-3";
         String expected3 = String.format(LOG_FORMAT, "WARN ", message);
         log.warn(message3);
-        String message4 = "test-warn-4-{}";
+        String message4 = "test-warn-4-{0}";
         String formatted4 = "test-warn-4-test";
         String expected4 = String.format(LOG_FORMAT, "WARN ", formatted4);
         log.warn(message4, () -> "test");
@@ -172,14 +172,14 @@ class Slf4jAdapterTest {
         Object message1 = "test-fatal-1";
         String expected1 = String.format(LOG_FORMAT, "ERROR", message);
         log.fatal(message1);
-        String message2 = "test-fatal-2-{}";
+        String message2 = "test-fatal-2-{0}";
         String formatted2 = "test-fatal-2-test";
         String expected2 = String.format(LOG_FORMAT, "ERROR", formatted2);
         log.fatal(message2, "test");
         String message3 = "test-fatal-3";
         String expected3 = String.format(LOG_FORMAT, "ERROR", message);
         log.fatal(message3);
-        String message4 = "test-fatal-4-{}";
+        String message4 = "test-fatal-4-{0}";
         String formatted4 = "test-fatal-4-test";
         String expected4 = String.format(LOG_FORMAT, "ERROR", formatted4);
         log.fatal(message4, () -> "test");
@@ -200,14 +200,14 @@ class Slf4jAdapterTest {
         Object message1 = "test-info-1";
         String expected1 = String.format(LOG_FORMAT, "INFO ", message);
         log.info(message1);
-        String message2 = "test-info-2-{}";
+        String message2 = "test-info-2-{0}";
         String formatted2 = "test-info-2-test";
         String expected2 = String.format(LOG_FORMAT, "INFO ", formatted2);
         log.info(message2, "test");
         String message3 = "test-info-3";
         String expected3 = String.format(LOG_FORMAT, "INFO ", message);
         log.info(message3);
-        String message4 = "test-info-4-{}";
+        String message4 = "test-info-4-{0}";
         String formatted4 = "test-info-4-test";
         String expected4 = String.format(LOG_FORMAT, "INFO ", formatted4);
         log.info(message4, () -> "test");
