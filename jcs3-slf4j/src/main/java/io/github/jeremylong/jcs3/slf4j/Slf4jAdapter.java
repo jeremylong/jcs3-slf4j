a20cd151db0fb97d2a0b1d9a445c1dce89172910
Jeremy Long <jeremy.long@gmail.com>
2023-11-23 07:56:24 -0500

    fix: allow logger to be muted

--- jcs3-slf4j/src/main/java/io/github/jeremylong/jcs3/slf4j/Slf4jAdapter.java
+++ jcs3-slf4j/src/main/java/io/github/jeremylong/jcs3/slf4j/Slf4jAdapter.java
@@ -17,6 +17,7 @@ package io.github.jeremylong.jcs3.slf4j;
 
 import org.apache.commons.jcs3.log.Log;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
 
 import java.text.MessageFormat;
@@ -33,96 +34,133 @@ import java.util.stream.Collectors;
 public class Slf4jAdapter implements Log {
 
     private final Logger logger;
+    private final boolean muted;
+
+    public Slf4jAdapter() {
+        logger = LoggerFactory.getLogger(Slf4jAdapter.class);
+        muted = Boolean.getBoolean(System.getProperty("jcs.logSystem.mute", "false"));
+    }
 
     public Slf4jAdapter(Logger log) {
         this.logger = log;
+        muted = Boolean.getBoolean(System.getProperty("jcs.logSystem.mute", "false"));
     }
 
     @Override
     public void debug(String string) {
-        logger.debug(string);
+        if (!muted) {
+            logger.debug(string);
+        }
     }
 
     @Override
     public void debug(Object o) {
-        logger.debug(o.toString());
+        if (!muted) {
+            logger.debug(o.toString());
+        }
     }
 
     @Override
     public void debug(String string, Object... os) {
-        String msg = MessageFormat.format(string, os);
-        logger.debug(msg);
+        if (!muted) {
+            String msg = MessageFormat.format(string, os);
+            logger.debug(msg);
+        }
     }
 
     @Override
     public void debug(String string, Supplier<?>... splrs) {
-        List<String> args = Arrays.stream(splrs).map(it -> it.get().toString()).collect(Collectors.toList());
-        String msg = MessageFormat.format(string, args.toArray());
-        // log(Level.DEBUG, string, splrs);
-        logger.debug(msg);
+        if (!muted) {
+            List<String> args = Arrays.stream(splrs).map(it -> it.get().toString()).collect(Collectors.toList());
+            String msg = MessageFormat.format(string, args.toArray());
+            // log(Level.DEBUG, string, splrs);
+            logger.debug(msg);
+        }
     }
 
     @Override
     public void debug(String string, Throwable thrwbl) {
-        logger.debug(string, thrwbl);
+        if (!muted) {
+            logger.debug(string, thrwbl);
+        }
     }
 
     @Override
     public void error(String string) {
-        logger.error(string);
+        if (!muted) {
+            logger.error(string);
+        }
     }
 
     @Override
     public void error(Object o) {
-        logger.error(o.toString());
+        if (!muted) {
+            logger.error(o.toString());
+        }
     }
 
     @Override
     public void error(String string, Object... os) {
-        String msg = MessageFormat.format(string, os);
-        logger.error(msg);
+        if (!muted) {
+            String msg = MessageFormat.format(string, os);
+            logger.error(msg);
+        }
     }
 
     @Override
     public void error(String string, Supplier<?>... splrs) {
-        List<String> args = Arrays.stream(splrs).map(it -> it.get().toString()).collect(Collectors.toList());
-        String msg = MessageFormat.format(string, args.toArray());
-        logger.error(msg);
-        // log(Level.ERROR, string, splrs);
+        if (!muted) {
+            List<String> args = Arrays.stream(splrs).map(it -> it.get().toString()).collect(Collectors.toList());
+            String msg = MessageFormat.format(string, args.toArray());
+            logger.error(msg);
+            // log(Level.ERROR, string, splrs);
+        }
     }
 
     @Override
     public void error(String string, Throwable thrwbl) {
-        logger.error(string, thrwbl);
+        if (!muted) {
+            logger.error(string, thrwbl);
+        }
     }
 
     @Override
     public void fatal(String string) {
-        logger.error(string);
+        if (!muted) {
+            logger.error(string);
+        }
     }
 
     @Override
     public void fatal(Object o) {
-        logger.error(o.toString());
+        if (!muted) {
+            logger.error(o.toString());
+        }
     }
 
     @Override
     public void fatal(String string, Object... os) {
-        String msg = MessageFormat.format(string, os);
-        logger.error(msg);
+        if (!muted) {
+            String msg = MessageFormat.format(string, os);
+            logger.error(msg);
+        }
     }
 
     @Override
     public void fatal(String string, Supplier<?>... splrs) {
-        List<String> args = Arrays.stream(splrs).map(it -> it.get().toString()).collect(Collectors.toList());
-        String msg = MessageFormat.format(string, args.toArray());
-        logger.error(msg);
-        // log(Level.ERROR, string, splrs);
+        if (!muted) {
+            List<String> args = Arrays.stream(splrs).map(it -> it.get().toString()).collect(Collectors.toList());
+            String msg = MessageFormat.format(string, args.toArray());
+            logger.error(msg);
+            // log(Level.ERROR, string, splrs);
+        }
     }
 
     @Override
     public void fatal(String string, Throwable thrwbl) {
-        logger.error(string, thrwbl);
+        if (!muted) {
+            logger.error(string, thrwbl);
+        }
     }
 
     @Override
@@ -132,151 +170,184 @@ public class Slf4jAdapter implements Log {
 
     @Override
     public void info(String string) {
-        logger.info(string);
+        if (!muted) {
+            logger.info(string);
+        }
     }
 
     @Override
     public void info(Object o) {
-        logger.info(o.toString());
+        if (!muted) {
+            logger.info(o.toString());
+        }
     }
 
     @Override
     public void info(String string, Object... os) {
-        String msg = MessageFormat.format(string, os);
-        logger.info(msg);
+        if (!muted) {
+            String msg = MessageFormat.format(string, os);
+            logger.info(msg);
+        }
     }
 
     @Override
     public void info(String string, Supplier<?>... splrs) {
-        List<String> args = Arrays.stream(splrs).map(it -> it.get().toString()).collect(Collectors.toList());
-        String msg = MessageFormat.format(string, args.toArray());
-        logger.info(msg);
-        // log(Level.INFO, string, splrs);
+        if (!muted) {
+            List<String> args = Arrays.stream(splrs).map(it -> it.get().toString()).collect(Collectors.toList());
+            String msg = MessageFormat.format(string, args.toArray());
+            logger.info(msg);
+            // log(Level.INFO, string, splrs);
+        }
     }
 
     @Override
     public void info(String string, Throwable thrwbl) {
-        logger.info(string, thrwbl);
+        if (!muted) {
+            logger.info(string, thrwbl);
+        }
     }
 
     @Override
     public boolean isDebugEnabled() {
-        return logger.isDebugEnabled();
+        return !muted && logger.isDebugEnabled();
     }
 
     @Override
     public boolean isErrorEnabled() {
-        return logger.isErrorEnabled();
+        return !muted && logger.isErrorEnabled();
     }
 
     @Override
     public boolean isFatalEnabled() {
-        return logger.isErrorEnabled();
+        return !muted && logger.isErrorEnabled();
     }
 
     @Override
     public boolean isInfoEnabled() {
-        return logger.isInfoEnabled();
+        return !muted && logger.isInfoEnabled();
     }
 
     @Override
     public boolean isTraceEnabled() {
-        return logger.isTraceEnabled();
+        return !muted && logger.isTraceEnabled();
     }
 
     @Override
     public boolean isWarnEnabled() {
-        return logger.isWarnEnabled();
+        return !muted && logger.isWarnEnabled();
     }
 
     @Override
     public void trace(String string) {
-        logger.trace(string);
+        if (!muted) {
+            logger.trace(string);
+        }
     }
 
     @Override
     public void trace(Object o) {
-        logger.trace(o.toString());
+        if (!muted) {
+            logger.trace(o.toString());
+        }
     }
 
     @Override
     public void trace(String string, Object... os) {
-        String msg = MessageFormat.format(string, os);
-        logger.trace(msg);
+        if (!muted) {
+            String msg = MessageFormat.format(string, os);
+            logger.trace(msg);
+        }
     }
 
     @Override
     public void trace(String string, Supplier<?>... splrs) {
-        List<String> args = Arrays.stream(splrs).map(it -> it.get().toString()).collect(Collectors.toList());
-        String msg = MessageFormat.format(string, args.toArray());
-        logger.trace(msg);
-        // log(Level.TRACE, string, splrs);
+        if (!muted) {
+            List<String> args = Arrays.stream(splrs).map(it -> it.get().toString()).collect(Collectors.toList());
+            String msg = MessageFormat.format(string, args.toArray());
+            logger.trace(msg);
+            // log(Level.TRACE, string, splrs);
+        }
     }
 
     @Override
     public void trace(String string, Throwable thrwbl) {
-        logger.trace(string, thrwbl);
+        if (!muted) {
+            logger.trace(string, thrwbl);
+        }
     }
 
     @Override
     public void warn(String string) {
-        logger.warn(string);
+        if (!muted) {
+            logger.warn(string);
+        }
     }
 
     @Override
     public void warn(Object o) {
-        logger.warn(o.toString());
+        if (!muted) {
+            logger.warn(o.toString());
+        }
     }
 
     @Override
     public void warn(String string, Object... os) {
-        String msg = MessageFormat.format(string, os);
-        logger.warn(msg);
+        if (!muted) {
+            String msg = MessageFormat.format(string, os);
+            logger.warn(msg);
+        }
     }
 
     @Override
     public void warn(String string, Supplier<?>... splrs) {
-        List<String> args = Arrays.stream(splrs).map(it -> it.get().toString()).collect(Collectors.toList());
-        String msg = MessageFormat.format(string, args.toArray());
-        logger.warn(msg);
-        // log(Level.WARN, string, splrs);
+        if (!muted) {
+            List<String> args = Arrays.stream(splrs).map(it -> it.get().toString()).collect(Collectors.toList());
+            String msg = MessageFormat.format(string, args.toArray());
+            logger.warn(msg);
+            // log(Level.WARN, string, splrs);
+        }
     }
 
     @Override
     public void warn(String string, Throwable thrwbl) {
-        logger.warn(string, thrwbl);
+        if (!muted) {
+            logger.warn(string, thrwbl);
+        }
     }
 
     private void log(Level level, String string, Supplier<?>[] splrs) {
-        if (isEnabled(level)) {
-            Object[] o = null;
-            if (splrs != null) {
-                o = Arrays.stream(splrs).map(s -> s.get()).toArray();
-            }
-            switch (level) {
-                case ERROR:
-                    logger.error(string, o);
-                    break;
-                case WARN:
-                    logger.warn(string, o);
-                    break;
-                case INFO:
-                    logger.info(string, o);
-                    break;
-                case TRACE:
-                    logger.trace(string, o);
-                    break;
-                case DEBUG:
-                    logger.debug(string, o);
-                    break;
+        if (!muted) {
+            if (isEnabled(level)) {
+                Object[] o = null;
+                if (splrs != null) {
+                    o = Arrays.stream(splrs).map(s -> s.get()).toArray();
+                }
+                switch (level) {
+                    case ERROR:
+                        logger.error(string, o);
+                        break;
+                    case WARN:
+                        logger.warn(string, o);
+                        break;
+                    case INFO:
+                        logger.info(string, o);
+                        break;
+                    case TRACE:
+                        logger.trace(string, o);
+                        break;
+                    case DEBUG:
+                        logger.debug(string, o);
+                        break;
+                }
             }
         }
     }
 
     private boolean isEnabled(Level level) {
-        return (Level.DEBUG == level && logger.isDebugEnabled()) || (Level.INFO == level && logger.isInfoEnabled())
-                || (Level.TRACE == level && logger.isTraceEnabled()) || (Level.WARN == level && logger.isWarnEnabled())
-                || (Level.ERROR == level && logger.isErrorEnabled());
+        return !muted && ((Level.DEBUG == level && logger.isDebugEnabled())
+                || (Level.INFO == level && logger.isInfoEnabled()) || (Level.TRACE == level && logger.isTraceEnabled())
+                || (Level.WARN == level && logger.isWarnEnabled())
+                || (Level.ERROR == level && logger.isErrorEnabled()));
     }
 
 }
